8f61f07ccfc6b6d2a22087adc23fbd3334b30745
PURBASHIS BEHERA <purbashis01042000@gmail.com>
2023-06-14 17:00:37 +0530

    Majority_element  in an array
    upgrad Assignment -1

---
+++ Moj_ele.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Moj_ele {
+     public static int majorityElement(int []v) {
+        //size of the given array:
+        int n = v.length;
+
+        for (int i = 0; i < n; i++) {
+            //selected element is v[i]
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                // counting the frequency of v[i]
+                if (v[j] == v[i]) {
+                    cnt++;
+                }
+            }
+
+            // check if frquency is greater than n/2:
+            if (cnt > (n / 2))
+                return v[i];
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the length of an array");
+        int l=sc.nextInt();
+        int[] arr = new int[l];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int ans = majorityElement(arr);
+        System.out.println("The majority element is: " + ans);
+
+    }
+
+} 
